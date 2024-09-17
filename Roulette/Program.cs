2cc4375e006a1aec95b84102f8335e36c5be5e35
12JE0527 <12JE0527@KB-PF3AL0KZ>
2024-09-17 10:30:58 +0200

    Add project files.

---
+++ Roulette/Program.cs
@@ -0,0 +1,10 @@
+﻿bool inRoulette = false;
+bool userExit = false;
+bool inDebt = false;
+int money = 100;
+int bet = 0;
+while (userExit == false)
+{
+    Console.WriteLine("------------------Welcome to the casino!------------------");
+    Console.WriteLine("               What would you like to play?               "
+}
